028e747ebff17a2c643e6ceebed52f2aab5af9d7
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-04-04 11:26:04 +0530

    reduct validations and try catch added

--- src/services/userServices/userservice.js
+++ src/services/userServices/userservice.js
@@ -3,12 +3,25 @@ const userModel = require("../../models/userModel");
 // Regestration service
 function checkEmail(email)
 {
+    try{
    return userModel.findOne(email)
+    }
+    catch(error)
+    {
+        return res.status(500).send({ status: false, message: error.message });
+    }
 }
 // data Creation
 function createData(data)
 {
+    try{
     return userModel.create(data)
+    }
+    catch(error)
+    {
+        return res.status(500).send({ status: false, message: error.message });
+    }
+
 }
 
 
